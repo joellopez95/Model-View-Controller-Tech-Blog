1ae7427eff0a93de4a256f9399ccf6362471c35c
Joel Lopez <jolopez.satx@gmail.com>
2023-11-26 15:32:35 -0600

    fixed file structure, boiler plate code for server, connection, db, worked on css and handlebars

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const sequelize = require('./config/connection'); 
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up session
+const sess = {
+  secret: 'top secret', // Change this to a secure, random string
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+};
+
+app.use(session(sess));
+
+// Set up Handlebars.js engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Use the routes defined in the controllers
+app.use(routes);
+
+// Sync Sequelize models to the database, then start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
